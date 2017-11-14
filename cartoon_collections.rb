25d11e501645268d43e22197aa3d60c62a02ab40
Tyler Pulse <tyler3321@gmail.com>
2017-11-14 21:42:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 
 def find_the_cheese(cheesefinder)
   cheese_types = ["cheddar", "gouda", "camembert"]
-      cheesefinder.any? do |word|
+      cheesefinder.all? do |word|
       cheese_types.find {|cheese| cheese == word}
     end
 end
